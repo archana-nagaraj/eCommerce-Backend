66f32d7272cdaa699b0dc68c3e3bface5dc96031
Archana Nagaraja <johndoe@example.com>
2021-03-06 16:19:37 -0800

    added product -routes, commented line 77

--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -74,7 +74,7 @@ router.post('/', (req, res) => {
       product_name: req.body.product_name,
       price = req.body.price,
       stock: req.body.stock,
-      category_id: req.body.category_id,
+      //category_id: req.body.category_id,
       tagIds: req.body.tag_id
     })
     .then((product) => {
