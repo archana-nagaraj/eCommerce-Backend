1e0f6be2a5d1a37441fd08d8ed0fcfbf95c80151
Archana Nagaraja <johndoe@example.com>
2021-03-06 16:34:59 -0800

    added sequelize dependencies, sequelize sync to db

--- Develop/server.js
+++ Develop/server.js
@@ -1,16 +1,26 @@
+// Dependencies
+// =============================================================
 const express = require('express');
 const routes = require('./routes');
 // import sequelize connection
+const sequelize = require('./config/connection');
 
+// Sets up the Express App
+// =============================================================
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+// Sets up the Express app to handle data parsing
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// Routes
+// =============================================================
 app.use(routes);
 
 // sync sequelize models to the database, then turn on the server
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}!`);
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => {
+      console.log(`App listening on port ${PORT}!`);
+    });
 });
